7b5c9378a9b74838acffc5280791be1f7eca3a67
李聪 <lc182540@163.com>
2021-03-03 10:36:35 +0800

    fix:打印

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const HOST = process.env.NODE_ENV === 'production' ? '172.22.22.31' : '127.0.0.1'
 const PORT = process.env.NODE_ENV === 'production' ? '3307' : '3306'
-
+console.log(HOST, PORT)
 const config = {
   port: 3000,
   database: {
