1d4b568a26e8ee71be27bcef81fb374eca855b02
李聪 <lc182540@163.com>
2021-03-03 14:23:22 +0800

    fix:端口号

--- config/index.js
+++ config/index.js
@@ -1,14 +1,11 @@
 const HOST = process.env.NODE_ENV === 'production' ? '172.22.22.31' : '127.0.0.1'
-const PORT = process.env.NODE_ENV === 'production' ? '3307' : '3306'
-console.log(process.env.NODE_ENV)
-console.log(HOST, PORT)
 const config = {
   port: 3000,
   database: {
     DATABASE: 'lc_blog',
     USERNAME: 'root',
     PASSWORD: 'mysql',
-    PORT,
+    PORT: '3306',
     HOST
   }
 }
