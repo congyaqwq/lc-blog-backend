d1ab08d237bb0b18ed58608c7e941fbd26b82467
李聪 <lc182540@163.com>
2021-03-24 17:48:21 +0800

    fix:api

--- routes/git.js
+++ routes/git.js
@@ -1,5 +1,5 @@
 const Router = require('koa-router')
-const router = new Router({ prefix: '/api/git' })
+const router = new Router({ prefix: '/git' })
 const gitpull = require('../controllers/git')
 
 router.post('/pull', gitpull)
