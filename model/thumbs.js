233f908338f3f1c37470233038b28678a70e70c6
李聪 <lc182540@163.com>
2021-04-08 12:04:10 +0800

    fix:判断重复点赞

--- model/thumbs.js
+++ model/thumbs.js
@@ -1,7 +1,11 @@
 const query = require('../mysql')
 
 class BlogModels {
-  async thumb({ blog_id, user_id }) {
+  async thumb({ blog_id, user_id = "" }) {
+    // 检查是否已经点赞过
+    let _checkSql = `SELECT user_id FROM thumb WHERE user_id = '${user_id}' AND blog_id = '${blog_id}';`
+    const res = await query(_checkSql, { blog_id, user_id })
+    if (res && res.length) return { error: '已经点赞过' }
     let _sql = `INSERT INTO thumb SET ?;`
     return await query(_sql, { blog_id, user_id })
   }
