0fd5263e738573b248ea274d66f265477dc6173b
cong <lc182540@163.com>
2021-11-22 17:04:41 +0800

    fix: front count bug

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,54 +1,65 @@
-const { total, list, orderList, add, update, detail, remove, addViews } = require('../model/blogs')
+const {
+  total,
+  frontTotal,
+  list,
+  orderList,
+  add,
+  update,
+  detail,
+  remove,
+  addViews,
+} = require("../model/blogs");
 
 exports.blogList = async (ctx) => {
-  let blogList = []
-  const user_id = ctx.cookies.get('uid')
-  if (ctx.request.url.includes('frontlist')) {
-    blogList = await orderList({ ...ctx.query, user_id })
+  let blogList = [];
+  const user_id = ctx.cookies.get("uid");
+  let count = 0;
+  if (ctx.request.url.includes("frontlist")) {
+    blogList = await orderList({ ...ctx.query, user_id });
+    const countObj = await frontTotal({ ...ctx.query });
+    count = countObj[0].count;
   } else {
-    blogList = await list({ ...ctx.query, user_id })
-  }
-  const countObj = await total({ ...ctx.query })
-  ctx.body = {
-    total: countObj[0].count,
-    list: blogList
+    blogList = await list({ ...ctx.query, user_id });
+    const countObj = await total({ ...ctx.query });
+    count = countObj[0].count;
   }
 
-}
+  ctx.body = {
+    total: count,
+    list: blogList,
+  };
+};
 
 exports.blogAdd = async (ctx) => {
-  const { title, content } = ctx.request.body
+  const { title, content } = ctx.request.body;
   if (!title || !content) {
-    ctx.throw(401, '参数错误')
+    ctx.throw(401, "参数错误");
   }
-  if (ctx.request.body)
-    ctx.body = await add(ctx.request.body)
-}
+  if (ctx.request.body) ctx.body = await add(ctx.request.body);
+};
 
 exports.blogUpdate = async (ctx) => {
-  const { id } = ctx.params
-  if (ctx.request.body)
-    ctx.body = await update(ctx.request.body, id)
-}
+  const { id } = ctx.params;
+  if (ctx.request.body) ctx.body = await update(ctx.request.body, id);
+};
 
 exports.blogDetail = async (ctx) => {
-  let { authorization: token } = ctx.request.header
-  let { id } = ctx.params
-  id = Number(id)
-  const user_id = ctx.cookies.get('uid')
+  let { authorization: token } = ctx.request.header;
+  let { id } = ctx.params;
+  id = Number(id);
+  const user_id = ctx.cookies.get("uid");
   if (!token) {
-    await addViews(id)
+    await addViews(id);
   }
-  const data = await detail(id, user_id)
+  const data = await detail(id, user_id);
   if (!data) {
-    ctx.throw('404', '未找到该条数据')
+    ctx.throw("404", "未找到该条数据");
   }
-  ctx.body = data
-}
+  ctx.body = data;
+};
 
 exports.blogRemove = async (ctx) => {
-  const { id } = ctx.params
-  const data = await remove(id)
-  ctx.body = data
-}
-
+  const { id } = ctx.params;
+  const data = await remove(id);
+  ctx.body = data;
+};
--- model/blogs.js
+++ model/blogs.js
@@ -4,92 +4,138 @@ const knex = require('../mysql')
 
 class BlogModels {
   async total(values) {
-    const { keyword = "" } = values
-    return knex('blog').count({ 'count': '*' }).where('title', 'like', `%${keyword}%`).orWhere('content', 'like', `%${keyword}%`)
+    const { keyword = "" } = values;
+    return knex("blog")
+      .count({ count: "*" })
+      .where("title", "like", `%${keyword}%`)
+      .orWhere("content", "like", `%${keyword}%`);
+  }
+  async frontTotal(values) {
+    const { keyword = "" } = values;
+    return knex("blog")
+      .count({ count: "*" })
+      .where(function(){
+        this.where("title", "like", `%${keyword}%`).orWhere("content", "like", `%${keyword}%`)
+      }).andWhere("status", "=", "1");
   }
   async add(values) {
-    return knex('blog').insert([{
-      ...values
-    }])
+    return knex("blog").insert([
+      {
+        ...values,
+      },
+    ]);
   }
   async list({ page = 1, per_page = 12, ...values }) {
-    const { keyword = "", tags = '' } = values
-    let res = knex.select('id', "created_time", "title", "thumbs", "views", "tags", "sort", "status", "content")
-      .from('blog')
-      .limit(per_page).offset((page - 1) * per_page)
+    const { keyword = "", tags = "" } = values;
+    let res = knex
+      .select(
+        "id",
+        "created_time",
+        "title",
+        "thumbs",
+        "views",
+        "tags",
+        "sort",
+        "status",
+        "content"
+      )
+      .from("blog")
+      .limit(per_page)
+      .offset((page - 1) * per_page);
 
     if (keyword) {
-      res.where('title', 'like', `%${keyword}%`).orWhere('content', 'like', `%${keyword}%`)
+      res
+        .where("title", "like", `%${keyword}%`)
+        .orWhere("content", "like", `%${keyword}%`);
     }
     if (tags) {
-      res.whereRaw(`FIND_IN_SET(${tags},tags)`)
+      res.whereRaw(`FIND_IN_SET(${tags},tags)`);
     }
-    res = await res
-    return await Promise.all(res.map(async it => {
-      return {
-        ...it,
-        tags: it.tags ? await listById(it.tags) : '',
-        content: it.content.substr(0, 100)
-      }
-    }))
+    res = await res;
+    return await Promise.all(
+      res.map(async (it) => {
+        return {
+          ...it,
+          tags: it.tags ? await listById(it.tags) : "",
+          content: it.content.substr(0, 100),
+        };
+      })
+    );
   }
   async orderList({ page = 1, per_page = 12, ...values }) {
-    const { keyword = "", user_id, tags = '' } = values
-    let mysql = knex.select('id', "created_time", "title", "thumbs", "views", "tags", "sort",  "content")
-      .from('blog')
-      .andWhere('status', '=', '1')
-      .limit(per_page).offset((page - 1) * per_page)
+    const { keyword = "", user_id, tags = "" } = values;
+    let mysql = knex
+      .select(
+        "id",
+        "created_time",
+        "title",
+        "thumbs",
+        "views",
+        "tags",
+        "sort",
+        "content"
+      )
+      .from("blog")
+      .andWhere("status", "=", "1")
+      .limit(per_page)
+      .offset((page - 1) * per_page);
     if (keyword) {
-      mysql.where('title', 'like', `%${keyword}%`).orWhere('content', 'like', `%${keyword}%`)
+      mysql
+        .where("title", "like", `%${keyword}%`)
+        .orWhere("content", "like", `%${keyword}%`);
     }
     if (tags) {
-      mysql.whereRaw(`FIND_IN_SET(${tags},tags)`)
+      mysql.whereRaw(`FIND_IN_SET(${tags},tags)`);
     }
-    let res = await mysql
-    let hasThumbList = []
+    let res = await mysql;
+    let hasThumbList = [];
     if (user_id) {
-      hasThumbList = await thumbList(user_id)
+      hasThumbList = await thumbList(user_id);
     }
-    hasThumbList = hasThumbList.map(it => it.blog_id)
-    return await Promise.all(res.map(async it => {
-      return {
-        ...it,
-        is_thumb: Number(hasThumbList.includes(it.id)),
-        tags: it.tags ? await listById(it.tags) : '',
-        content: it.content.substr(0, 100)
-      }
-    }))
+    hasThumbList = hasThumbList.map((it) => it.blog_id);
+    return await Promise.all(
+      res.map(async (it) => {
+        return {
+          ...it,
+          is_thumb: Number(hasThumbList.includes(it.id)),
+          tags: it.tags ? await listById(it.tags) : "",
+          content: it.content.substr(0, 100),
+        };
+      })
+    );
   }
   async update(values, id) {
-    const { title, content, tags, status } = values
-    let val = { title, content, tags, status: Number(status) }
-    const newVal = {}
-    Object.keys(val).map(it => {
+    const { title, content, tags, status } = values;
+    let val = { title, content, tags, status: Number(status) };
+    const newVal = {};
+    Object.keys(val).map((it) => {
       if (val[it] || val[it] === 0) {
-        newVal[it] = val[it]
+        newVal[it] = val[it];
       }
-    })
-    return knex('blog').where({ id }).update({
-      ...newVal
-    })
+    });
+    return knex("blog")
+      .where({ id })
+      .update({
+        ...newVal,
+      });
   }
   async detail(id, user_id) {
-    const res = await knex('blog').where({ id }).limit(1)
-    let hasThumbList = []
+    const res = await knex("blog").where({ id }).limit(1);
+    let hasThumbList = [];
     if (user_id) {
-      hasThumbList = await thumbList(user_id)
+      hasThumbList = await thumbList(user_id);
     }
-    hasThumbList = hasThumbList.map(it => it.blog_id)
-    const data = res[0] || {}
-    data.is_thumb = Number(hasThumbList.includes(id))
-    data.tags = await listById(data.tags)
-    return data
+    hasThumbList = hasThumbList.map((it) => it.blog_id);
+    const data = res[0] || {};
+    data.is_thumb = Number(hasThumbList.includes(id));
+    data.tags = await listById(data.tags);
+    return data;
   }
   async remove(id) {
-    return knex('blog').where({ id }).del()
+    return knex("blog").where({ id }).del();
   }
   async addViews(id) {
-    return knex('blog').where({ id }).increment('views', 1)
+    return knex("blog").where({ id }).increment("views", 1);
   }
 }
 
