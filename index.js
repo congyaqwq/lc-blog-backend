6e18aa529a64cdea177fa9b4626faabf34e21680
cong <cong.li@carbonnt.com>
2021-12-27 16:47:32 +0800

    fix: 暂时取消点赞

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ const CONFIG = {
   maxAge: 86400000 * 30,
   autoCommit: true /** (boolean) automatically commit headers (default true) */,
   overwrite: true /** (boolean) can overwrite or not (default true) */,
-  httpOnly: false /** (boolean) httpOnly or not (default true) */,
-  signed: false /** (boolean) signed or not (default true) */,
+  httpOnly: true /** (boolean) httpOnly or not (default true) */,
+  signed: true /** (boolean) signed or not (default true) */,
   rolling: false /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */,
   renew: false /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/,
-  secure: process.env.NODE_ENV === 'production'? true: false /** (boolean) secure cookie*/,
+  secure: false /** (boolean) secure cookie*/,
   sameSite: 'None',
   store: {
     set: (key, sess) => {
@@ -48,6 +48,7 @@ const CONFIG = {
 };
 
 app.use(session(CONFIG, app));
+app.proxy = true;
 
 app.use(async (ctx, next) => {
   if (ctx.cookies.get("uuid")) return await next();
